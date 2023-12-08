855d931bd9724fff22c549ce2dca6685d2ea7dc0
Onyi Dozie <onyiyechukwu.dozie@kcl.ac.uk>
2023-12-08 12:07:46 +0000

    fix

--- things/models.py
+++ things/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator,MinValueValidator
 # Create your models here.
+
 class Thing(models.Model):
     name = models.CharField(max_length=30,unique=True)
     description = models.CharField(unique = False,max_length=120,blank = True)
